0f04c3afd2521701d9e117fe173a095b42d6ada9
Enger Jimenez <ejimenez@oriontek.do>
2019-12-15 14:18:42 -0400

    first commit

---
+++ src/database/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "###YOUR-API-KEY####",
+    authDomain: "###YOUR-AUTH-DOMAIN",
+    databaseURL: "###YOUR-DATABASE###",
+    projectId: "###YOUR-PROJECT-ID###",
+    storageBucket: "###YOUR-STORAGE-URL###",
+    messagingSenderId: "###YOUR-MESSAGING-SENDER-ID###",
+    appId: "###YOUR-APP-ID###",
+    measurementId: "###YOUR-G-ANALYTICS-ID###"
+};
